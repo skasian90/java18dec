d9c42858c52fb1427021aee53d6f999dfd32ea2d
skasian90 <saimon_55@inbox.ru>
2018-12-24 20:20:45 +0600

    Решили лабораторная 1 задача 1 2 3

---
+++ src/лаб1/task1.java
@@ -0,0 +1,19 @@
+package лаб1;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("+---+");
+        System.out.println("|   |");
+        System.out.println("|   |");
+        System.out.println("+---+");
+        System.out.println("#");
+        System.out.println("##");
+        System.out.println("###");
+        System.out.println("####");
+        System.out.println(" /\\");
+        System.out.println("//\\\\");
+        System.out.println("//\\\\");
+        System.out.println(" ][");
+        System.out.println("{ \"x\":15, \"y\": 28.7 }");}
+}
+
