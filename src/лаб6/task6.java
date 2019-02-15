91c963359070399e329c1446cdf49bd5aecbf6f8
skasian90 <saimon_55@inbox.ru>
2019-02-15 17:25:27 +0600

    1

---
+++ src/лаб6/task6.java
@@ -0,0 +1,18 @@
+package лаб6;
+
+import java.util.Scanner;
+
+public class task6 {
+    public static void main(String[] args) {
+        //
+        int X;
+        int result;
+        Scanner s=new Scanner(System.in);
+        X = s.nextInt();
+        int i; //счетчик шагов
+        i = 10;
+
+        while (X <= i ) {
+            result = X + 3;}
+        System.out.println(result);
+    }}
